ea4a0f2d70fb84cdb615659ca702a0fe79122c97
Christopher Williams <cw@chriswillia.ms>
2019-06-16 14:14:52 -0500

    Adds a basic form with validation to add new todo items to the list.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import td from './TestData';
 import TodoContext from './TodoContext';
 import Header from './Header/Header';
+import Input from './Input/Input';
 import TodoList from './TodoList/TodoList';
 
 import './App.css';
@@ -15,7 +16,7 @@ class App extends React.Component {
       todoList: [],
     }
   }
-
+  
   componentDidMount() {
     // get the TestDat into state
     this.setState({
@@ -23,16 +24,23 @@ class App extends React.Component {
     });
   }
 
+  addNewTodoItem = newItem => {
+    let newTodoList = [...this.state.todoList, newItem];
+    this.setState({todoList: newTodoList});
+  }
+
   render() {
     // place state into context
     const contextValue = {
       todoList: this.state.todoList,
+      addItem: this.addNewTodoItem,
     }
 
     return (
       <div className='App'>
         <TodoContext.Provider value={contextValue}>
           <Header />
+          <Input />
           <TodoList />
         </TodoContext.Provider>
       </div>
